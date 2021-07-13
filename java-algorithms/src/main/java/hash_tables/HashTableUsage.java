e6135c89f65a95045119c23f38cf36533f0271b2
jpassgo <jeffpascoe9@gmail.com>
2021-07-12 20:28:40 -0500

    Testing

---
+++ java-algorithms/src/main/java/hash_tables/HashTableUsage.java
@@ -0,0 +1,10 @@
+package hash_tables;
+
+public class HashTableUsage {
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable();
+        hashTable.put("first", 1);
+        String first = hashTable.get("first");
+        System.out.println(first);
+    }
+}
