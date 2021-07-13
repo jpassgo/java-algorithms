af14e80b1f89c651a79948128a86796223c84616
jpassgo <jeffpascoe9@gmail.com>
2021-07-12 20:28:34 -0500

    Partial HashTable implementation

---
+++ java-algorithms/src/main/java/hash_tables/HashTable.java
@@ -0,0 +1,59 @@
+package hash_tables;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class HashTable {
+
+    private List<List<Node>> hashTable;
+
+    public HashTable() {
+        this.hashTable = new ArrayList<>(2000000000);
+    }
+
+    public <T> T get(String key) {
+        int index = hash(key);
+
+        T value = null;
+
+        for(Node node : hashTable.get(index)) {
+            if(node.getKey().equals(key)) {
+                value = (T) node.getValue();
+            }
+        }
+
+        return value;
+    }
+
+    public <T> int put(String key, T value) {
+        int index = hash(key);
+        if(index > hashTable.size()) {
+            resize(index);
+        }
+        try {
+            hashTable.get(index).add(new Node<T>(key, value));
+        } catch(IndexOutOfBoundsException e) {
+            resize(index);
+            hashTable.add(index, Arrays.asList(new Node<T>(key, value)));
+        }
+        return index;
+    }
+
+    private int resize(int newSize) {
+        for(int i = hashTable.size(); i < newSize; i++) {
+            hashTable.add(null);
+        }
+        return newSize;
+    }
+
+    private int hash(String key) {
+        int hash = 7;
+        for (int i = 0; i < key.length(); i++) {
+            hash = hash*31 + key.charAt(i);
+        }
+        return hash;
+    }
+}
