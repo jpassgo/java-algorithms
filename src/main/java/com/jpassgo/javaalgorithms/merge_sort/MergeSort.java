5dff365869be2e24bb343b7cb332d3b301fe09aa
jpassgo <jeffpascoe9@gmail.com>
2021-07-19 21:02:32 -0500

    Starting implementing merge sort.

---
+++ src/main/java/com/jpassgo/javaalgorithms/merge_sort/MergeSort.java
@@ -0,0 +1,33 @@
+package com.jpassgo.javaalgorithms.merge_sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] array, int left, int right) {
+        if (right <= left) return;
+        int mid = (left+right)/2;
+
+        mergeSort(array, left, mid);
+        mergeSort(array, mid+1, right);
+        merge(array, left, mid, right);
+    }
+
+    private void merge(int[] array, int left, int mid, int right) {
+
+        int leftArray[] = Arrays.copyOfRange(array, left, mid);
+        int rightArray[] = Arrays.copyOfRange(array, mid+1, right);
+
+        int leftLength = leftArray.length - 1;
+        int rightLength = rightArray.length - 1;
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        for (int i = left; i < right; i++) {
+            if(leftIndex < leftLength && rightIndex < rightLength) {
+
+            }
+        }
+    }
+}
