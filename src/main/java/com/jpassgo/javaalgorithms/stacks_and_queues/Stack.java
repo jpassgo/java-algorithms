cdc00cfb83b51e65a10ca9c8a7e6183b92b7b8e7
jpassgo <jeffpascoe9@gmail.com>
2021-07-24 18:44:14 -0500

    Implementing stack.

--- src/main/java/com/jpassgo/javaalgorithms/stacks_and_queues/Stack.java
+++ src/main/java/com/jpassgo/javaalgorithms/stacks_and_queues/Stack.java
@@ -2,30 +2,41 @@ package com.jpassgo.javaalgorithms.stacks_and_queues;
 
 import java.lang.reflect.Array;
 
-public class Stack<E> {
+public class Stack<E extends Comparable> {
 
     private E[] elements;
+    private int size;
 
     public Stack(Class<E> clazz) {
         elements = (E[]) Array.newInstance(clazz, 10000);
     }
 
     public void push(E element) {
+        elements[size++] = element;
     }
 
     public boolean empty() {
-        return false;
+        if(size == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public <E> int search(E o) {
-        return 0;
+    public <E extends Comparable> int search(E o) throws Exception {
+        for (int i = 0; i <= size; i++) {
+            if(elements[i].compareTo(o) == 0) {
+                return i;
+            }
+        }
+        throw new Exception("Element not found");
     }
 
     public E peak() {
-        return null;
+        return elements[size];
     }
 
     public E pop() {
-        return null;
+        return elements[size--];
     }
 }
