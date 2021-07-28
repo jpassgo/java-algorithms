b281e8d36cabfb7fea18ba4f0cccef1083b408ee
jpassgo <jeffpascoe9@gmail.com>
2021-07-27 20:38:20 -0500

    Spotless

--- src/main/java/com/jpassgo/javaalgorithms/hash_tables/HashTableUsage.java
+++ src/main/java/com/jpassgo/javaalgorithms/hash_tables/HashTableUsage.java
@@ -1,10 +1,10 @@
 package com.jpassgo.javaalgorithms.hash_tables;
 
 public class HashTableUsage {
-    public static void main(String[] args) {
-        HashTable hashTable = new HashTable();
-        hashTable.put("first", 1);
-        Integer first = hashTable.get("first");
-        System.out.println(first);
-    }
+  public static void main(String[] args) {
+    HashTable hashTable = new HashTable();
+    hashTable.put("first", 1);
+    Integer first = hashTable.get("first");
+    System.out.println(first);
+  }
 }
