143c05e8db00d529b0a2425c3608c45e5f408b96
jpassgo <jeffpascoe9@gmail.com>
2021-07-30 20:43:58 -0500

    Implementing a generic Pair class to contain a pair of values.

---
+++ src/main/java/com/jpassgo/javaalgorithms/random_problems/Pair.java
@@ -0,0 +1,19 @@
+package com.jpassgo.javaalgorithms.random_problems;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pair<T> {
+    T first;
+    T second;
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
