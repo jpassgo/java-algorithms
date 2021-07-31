155f40fed358cd7ea3ea1515a25f38c2370e792d
jpassgo <jeffpascoe9@gmail.com>
2021-07-30 20:42:18 -0500

    Implementing algorithm that prints all positive integer solutions for a^3 + b^3 = c^3 + d^3 where a, b, c, d are all integers between 1 and 1000.

---
+++ src/main/java/com/jpassgo/javaalgorithms/random_problems/ThisEqualsThat.java
@@ -0,0 +1,25 @@
+package com.jpassgo.javaalgorithms.random_problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThisEqualsThat {
+
+    /**
+     * The Problem: Print all positive integer solutions for a^3 + b^3 = c^3 + d^3
+     * where a, b, c, d are all integers between 1 and 1000.
+     */
+
+    public static void main(String[] args) {
+        List<Pair> solutions = new ArrayList<Pair>();
+        for (Integer a = 0; a < 1000; a++) {
+            for (Integer b = 0; b < 1000; b++) {
+                solutions.add(new Pair(a, b));
+            }
+        }
+        for(Pair pair : solutions) {
+            System.out.println(pair);
+        }
+    }
+
+}
