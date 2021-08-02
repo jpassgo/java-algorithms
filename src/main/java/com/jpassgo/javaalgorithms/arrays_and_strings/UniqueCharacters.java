9c1d73b688b8f255e11dc19cb295107945aeba12
jpassgo <jeffpascoe9@gmail.com>
2021-08-01 19:10:26 -0500

    spotless

--- src/main/java/com/jpassgo/javaalgorithms/arrays_and_strings/UniqueCharacters.java
+++ src/main/java/com/jpassgo/javaalgorithms/arrays_and_strings/UniqueCharacters.java
@@ -1,32 +1,30 @@
 package com.jpassgo.javaalgorithms.arrays_and_strings;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 class UniqueCharacters {
 
-    public static void main(String[] args) {
-        String containsDuplicateCharacters = "bhujhbhybuhuibhuij";
-        String containsNoDuplicateCharacters = "asdfghjkl";
+  public static void main(String[] args) {
+    String containsDuplicateCharacters = "bhujhbhybuhuibhuij";
+    String containsNoDuplicateCharacters = "asdfghjkl";
 
-        UniqueCharacters uniqueCharacters = new UniqueCharacters();
+    UniqueCharacters uniqueCharacters = new UniqueCharacters();
 
-        System.out.println(uniqueCharacters.containsNoDuplicates(containsDuplicateCharacters));
-        System.out.println(uniqueCharacters.containsNoDuplicates(containsNoDuplicateCharacters));
+    System.out.println(uniqueCharacters.containsNoDuplicates(containsDuplicateCharacters));
+    System.out.println(uniqueCharacters.containsNoDuplicates(containsNoDuplicateCharacters));
+  }
+
+  public boolean containsNoDuplicates(String input) {
+    List<Character> charList = new ArrayList();
+
+    for (char c : input.toCharArray()) {
+      if (!charList.contains(c)) {
+        charList.add(c);
+      } else {
+        return false;
+      }
     }
-
-    public boolean containsNoDuplicates(String input) {
-        List<Character> charList = new ArrayList();
-
-        for(char c : input.toCharArray()) {
-            if(!charList.contains(c)) {
-                charList.add(c);
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 }
--- src/test/java/com/jpassgo/javaalgorithms/stacks_and_queues/StackTest.java
+++ src/test/java/com/jpassgo/javaalgorithms/stacks_and_queues/StackTest.java
@@ -4,17 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
+import static java.lang.Double.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class StackTest {
 
-    @Test
-    void pop() {
-        Stack stack = new Stack();
+  @Test
+  void pop() {
+    Stack stack = new Stack();
 
-        stack.push(valueOf("100"));
+    stack.push(valueOf("100"));
 
-        assertThat(stack.peak()).isEqualTo(Double.valueOf(100));
-    }
+    assertThat(stack.peak()).isEqualTo(valueOf(100));
+  }
 }
