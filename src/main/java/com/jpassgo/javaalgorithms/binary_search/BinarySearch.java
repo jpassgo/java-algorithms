a0be2f6d47df8a9da017e2fe72fc304f0c35e7a2
jpassgo <jeffpascoe9@gmail.com>
2021-08-09 17:19:58 -0500

    Spotless formatting.

--- src/main/java/com/jpassgo/javaalgorithms/binary_search/BinarySearch.java
+++ src/main/java/com/jpassgo/javaalgorithms/binary_search/BinarySearch.java
@@ -6,36 +6,35 @@ import static com.jpassgo.javaalgorithms.quick_sort.QuickSort.quickSort;
 
 public class BinarySearch {
 
-    public static void main(String[] args) {
-        int[] array = new int[24];
+  public static void main(String[] args) {
+    int[] array = new int[24];
 
-        Random random = new Random();
+    Random random = new Random();
 
-        for (int i = 0; i < 23; i++) {
-            array[i] = random.nextInt(999);
-        }
-
-        array[23] = 100;
-
-        quickSort(array, 0, array.length - 1);
-        System.out.println("Index of 100: " + binarySearch(array, 0, array.length - 1, 100));
-
-        for (int i = 0; i < 23; i++) {
-            System.out.println(array[i]);
-        }
-
+    for (int i = 0; i < 23; i++) {
+      array[i] = random.nextInt(999);
     }
 
-    public static int binarySearch(int[] array, int low, int high, int value) {
-        if(low > high) return -1;
+    array[23] = 100;
 
-        int mid = low + ((high - low) / 2);
-        if(array[mid] > value) {
-            return binarySearch(array, low, mid-1, value);
-        } else if(array[mid] < value) {
-            return binarySearch(array, mid+1, high, value);
-        } else {
-            return mid;
-        }
+    quickSort(array, 0, array.length - 1);
+    System.out.println("Index of 100: " + binarySearch(array, 0, array.length - 1, 100));
+
+    for (int i = 0; i < 23; i++) {
+      System.out.println(array[i]);
     }
+  }
+
+  public static int binarySearch(int[] array, int low, int high, int value) {
+    if (low > high) return -1;
+
+    int mid = low + ((high - low) / 2);
+    if (array[mid] > value) {
+      return binarySearch(array, low, mid - 1, value);
+    } else if (array[mid] < value) {
+      return binarySearch(array, mid + 1, high, value);
+    } else {
+      return mid;
+    }
+  }
 }
--- src/main/java/com/jpassgo/javaalgorithms/permutations/ThisInThat.java
+++ src/main/java/com/jpassgo/javaalgorithms/permutations/ThisInThat.java
@@ -2,49 +2,47 @@ package com.jpassgo.javaalgorithms.permutations;
 
 public class ThisInThat {
 
-    /**
-     * How many permutations of a exist in b?
-     *
-     * Example:
-     * a: abbc
-     * b: cbabadcbbabbcbabaabccbabc
-     *
-     */
+  /**
+   * How many permutations of a exist in b?
+   *
+   * <p>Example: a: abbc b: cbabadcbbabbcbabaabccbabc
+   */
 
-    /**
-     * Loop over each character in the second string. As soon as you find a character that exists in the first string
-     * grab the index of the current string and then create a temporary copy of the first string. Remove the character
-     * that is found in both strings from the temporary copy and then rinse and repeat for the next 3 contiguous
-     * characters. Once done if the copy string is empty increment the counter.
-     */
-    public static void main(String[] args) {
-        String firstString = "abbc";
-        String secondString = "cbabadcbbabbcbabaabccbabc";
+  /**
+   * Loop over each character in the second string. As soon as you find a character that exists in
+   * the first string grab the index of the current string and then create a temporary copy of the
+   * first string. Remove the character that is found in both strings from the temporary copy and
+   * then rinse and repeat for the next 3 contiguous characters. Once done if the copy string is
+   * empty increment the counter.
+   */
+  public static void main(String[] args) {
+    String firstString = "abbc";
+    String secondString = "cbabadcbbabbcbabaabccbabc";
 
-        int permutationCount = 0;
+    int permutationCount = 0;
 
-        for(char currentChar : secondString.toCharArray()) {
-            String currentCharAsString = String.valueOf(currentChar);
+    for (char currentChar : secondString.toCharArray()) {
+      String currentCharAsString = String.valueOf(currentChar);
 
-            if(firstString.contains(currentCharAsString)) {
-                int indexOfCurrentCharacter = secondString.indexOf(currentChar);
+      if (firstString.contains(currentCharAsString)) {
+        int indexOfCurrentCharacter = secondString.indexOf(currentChar);
 
-                String copyOfFirstString = firstString;
-                copyOfFirstString = copyOfFirstString.replace(currentCharAsString, "");
+        String copyOfFirstString = firstString;
+        copyOfFirstString = copyOfFirstString.replace(currentCharAsString, "");
 
-                for (int i = indexOfCurrentCharacter; i < indexOfCurrentCharacter+3; i++) {
-                    String nextChar = String.valueOf(secondString.toCharArray()[i]);
-                    if(copyOfFirstString.contains(nextChar)) {
-                        copyOfFirstString = copyOfFirstString.replace(nextChar, "");
-                    }
-                }
-
-                if(copyOfFirstString.isBlank()) {
-                    permutationCount++;
-                }
-            }
+        for (int i = indexOfCurrentCharacter; i < indexOfCurrentCharacter + 3; i++) {
+          String nextChar = String.valueOf(secondString.toCharArray()[i]);
+          if (copyOfFirstString.contains(nextChar)) {
+            copyOfFirstString = copyOfFirstString.replace(nextChar, "");
+          }
         }
 
-        System.out.println("Number of permutations: " + permutationCount);
+        if (copyOfFirstString.isBlank()) {
+          permutationCount++;
+        }
+      }
     }
+
+    System.out.println("Number of permutations: " + permutationCount);
+  }
 }
