4a41b5e196c0aa5d2d09aff962b797af33ce9165
jpassgo <jeffpascoe9@gmail.com>
2021-08-13 21:34:27 -0500

    Test for binary search tree.

---
+++ src/test/java/com/jpassgo/javaalgorithms/trees/BinarySearchTreeTest.java
@@ -0,0 +1,36 @@
+package com.jpassgo.javaalgorithms.trees;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinarySearchTreeTest {
+
+    @Test
+    void insert() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        binarySearchTree.insert(100);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(1);
+        binarySearchTree.insert(123);
+        binarySearchTree.insert(34);
+        binarySearchTree.insert(5);
+    }
+
+    @Test
+    void contains() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        binarySearchTree.insert(100);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(1);
+        binarySearchTree.insert(123);
+        binarySearchTree.insert(34);
+        binarySearchTree.insert(5);
+
+        assertThat(binarySearchTree.contains(15)).isEqualTo(true);
+    }
+}
