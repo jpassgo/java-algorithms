32a3dcce86af4dc48161061cc844ff23439d7991
jpassgo <jeffpascoe9@gmail.com>
2021-08-15 15:52:27 -0500

    Adding ElementNotFoundException for when we don't find an element in a tree or other structure.

---
+++ src/main/java/com/jpassgo/javaalgorithms/trees/ElementNotFoundException.java
@@ -0,0 +1,22 @@
+package com.jpassgo.javaalgorithms.trees;
+
+public class ElementNotFoundException extends RuntimeException {
+    public ElementNotFoundException() {
+    }
+
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+
+    public ElementNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ElementNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ElementNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
