495718e265cfe891b9a67783d4c419ac04af67ac
jpassgo <jeffpascoe9@gmail.com>
2021-08-18 07:25:18 -0500

    Adding a new field to track the parent of the node.
    Also adding methods to determine if either a left or right child is present and if the node is itself a leaf.

--- src/main/java/com/jpassgo/javaalgorithms/trees/Node.java
+++ src/main/java/com/jpassgo/javaalgorithms/trees/Node.java
@@ -1,20 +1,21 @@
 package com.jpassgo.javaalgorithms.trees;
 
-public class Node<T extends Comparable<T>> {
+public class Node<T extends Comparable> {
 
     private Object value;
+    private Node parent;
     private Node leftChild;
     private Node rightChild;
 
-    public <T extends Comparable<T>> Node(T value) {
+    public <T extends Comparable> Node(T value) {
         this.value = value;
     }
 
-    public T getValue() {
-        return (T) value;
+    public <T extends Comparable<T>> T getValue() {
+        return (T) this.value;
     }
 
-    public <T extends Comparable<T>> void setValue(T value) {
+    public <T extends Comparable> void setValue(T value) {
         this.value = value;
     }
 
@@ -23,6 +24,7 @@ public class Node<T extends Comparable<T>> {
     }
 
     public void setLeftChild(Node leftChild) {
+        leftChild.setParent(this);
         this.leftChild = leftChild;
     }
 
@@ -31,6 +33,42 @@ public class Node<T extends Comparable<T>> {
     }
 
     public void setRightChild(Node rightChild) {
+        rightChild.setParent(this);
         this.rightChild = rightChild;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public boolean isLeftChildPresent() {
+        if(this.leftChild == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isRightChildPresent() {
+        if(this.rightChild == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isLeafNode() {
+        return !isLeftChildPresent() && !isRightChildPresent();
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", leftChild=" + leftChild +
+                ", rightChild=" + rightChild +
+                '}';
     }
 }
