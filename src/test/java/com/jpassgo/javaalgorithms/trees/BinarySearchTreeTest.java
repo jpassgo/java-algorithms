38d2c5a4fb71fd7db25ce8e63080397d8f91c163
jpassgo <jeffpascoe9@gmail.com>
2021-08-24 12:01:22 -0600

    Spotless formatting.

--- src/test/java/com/jpassgo/javaalgorithms/trees/BinarySearchTreeTest.java
+++ src/test/java/com/jpassgo/javaalgorithms/trees/BinarySearchTreeTest.java
@@ -1,56 +1,56 @@
 package com.jpassgo.javaalgorithms.trees;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class BinarySearchTreeTest {
 
-    @Test
-    void insert() {
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
+  @Test
+  void insert() {
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
 
-        binarySearchTree.insert(100);
-        binarySearchTree.insert(10);
-        binarySearchTree.insert(15);
-        binarySearchTree.insert(1);
-        binarySearchTree.insert(123);
-        binarySearchTree.insert(34);
-        binarySearchTree.insert(5);
-    }
+    binarySearchTree.insert(100);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(15);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(123);
+    binarySearchTree.insert(34);
+    binarySearchTree.insert(5);
+  }
 
-    @Test
-    void contains() {
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
+  @Test
+  void contains() {
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
 
-        binarySearchTree.insert(100);
-        binarySearchTree.insert(10);
-        binarySearchTree.insert(15);
-        binarySearchTree.insert(1);
-        binarySearchTree.insert(123);
-        binarySearchTree.insert(34);
-        binarySearchTree.insert(5);
+    binarySearchTree.insert(100);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(15);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(123);
+    binarySearchTree.insert(34);
+    binarySearchTree.insert(5);
 
-        assertThat(binarySearchTree.contains(15)).isEqualTo(true);
-    }
+    assertThat(binarySearchTree.contains(15)).isEqualTo(true);
+  }
 
-    @Test
-    void delete() {
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
+  @Test
+  void delete() {
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
 
-        binarySearchTree.insert(100);
-        binarySearchTree.insert(10);
-        binarySearchTree.insert(15);
-        binarySearchTree.insert(1);
-        binarySearchTree.insert(123);
-        binarySearchTree.insert(34);
-        binarySearchTree.insert(5);
+    binarySearchTree.insert(100);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(15);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(123);
+    binarySearchTree.insert(34);
+    binarySearchTree.insert(5);
 
-        binarySearchTree.preorder();
+    binarySearchTree.preorder();
 
-        System.out.println("******************************");
-        binarySearchTree.delete(15);
+    System.out.println("******************************");
+    binarySearchTree.delete(15);
 
-        binarySearchTree.preorder();
-    }
+    binarySearchTree.preorder();
+  }
 }
