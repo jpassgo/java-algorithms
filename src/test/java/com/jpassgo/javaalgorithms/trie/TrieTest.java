eb44b9ac54c245f3b4ff812e374da8f82e073c5f
jpassgo <jeffpascoe9@gmail.com>
2021-08-26 19:47:07 -0600

    Updating test.

--- src/test/java/com/jpassgo/javaalgorithms/trie/TrieTest.java
+++ src/test/java/com/jpassgo/javaalgorithms/trie/TrieTest.java
@@ -7,9 +7,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class TrieTest {
 
-    @Test
-    void insert() {
-        Trie trie = new Trie();
-        trie.insert("hello");
-    }
+  @Test
+  void insert() {
+    Trie trie = new Trie();
+    trie.insert("hello");
+  }
+
+  @Test
+  void print() {
+    Trie trie = new Trie();
+    trie.insert("hello");
+
+    System.out.println(trie.print());
+  }
+
 }
