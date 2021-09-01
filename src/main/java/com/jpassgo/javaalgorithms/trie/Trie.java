aa61d61103a8e7aadd6e9fba8c1a79507b0d025a
jpassgo <jeffpascoe9@gmail.com>
2021-08-31 19:44:23 -0500

    Re-implementing insert method of Trie class.

--- src/main/java/com/jpassgo/javaalgorithms/trie/Trie.java
+++ src/main/java/com/jpassgo/javaalgorithms/trie/Trie.java
@@ -21,48 +21,21 @@ public class Trie {
   }
 
   private void insert(Node node, char[] chars, int index) {
-    for (int i = 0; i < node.getChildCount(); i++) {
-      Node child = node.getChildren().get(i);
-      if(foundValueInTrie(chars, index, child)) {
-        insert(child, chars, ++index);
-      } else if(index < chars.length - 1) {
-        node.addChild(new Node(chars[index]));
-        index++;
-      }
+    Character character = null;
+    if(index <= chars.length - 1) {
+       character = Character.valueOf(chars[index]);
     }
 
-    if(node.getChildren().isEmpty() && index < chars.length) {
-      Node newChild = new Node(chars[index]);
-      node.addChild(newChild);
-      insert(newChild, chars, ++index);
+    if(node.getChildren().containsKey(Character.valueOf(chars[index]))) {
+      insert(node.getChildren().get(character), chars, ++index);
+    } else if(index < chars.length - 1) {
+      Node newNode = new Node(character);
+      node.addChild(newNode);
+      insert(newNode, chars, ++index);
+    } else if(index == chars.length - 1) {
+      Node newNode = new Node(character);
+      node.addChild(newNode);
+      node.setIsCompleteWord();
     }
-  }
-
-  public String print() {
-    return print(root, new StringBuilder());
-  }
-
-  private String print(Node current, StringBuilder sb) {
-    sb.append(current.getValue());
-    sb.append(current).append(NEW_LINE);
-    sb.append(LEFT_TREE).append(SPACING);
-
-    for (int i = 0; i < current.nonNullChildrenCount(); i++) {
-      sb.append(DIRECT_CHILD).append(SPACING);
-    }
-    sb.append(RIGHT_TREE).append(NEW_LINE);
-
-
-    for(Node child : current.getChildren()) {
-      if(child != null) {
-        sb.append(child.getValue()).append(SPACING);
-      }
-    }
-
-    return sb.toString();
-  }
-
-  private boolean foundValueInTrie(char[] chars, int index, Node current) {
-    return current != null && index < chars.length-1 && current.getValue() == chars[index];
   }
 }
