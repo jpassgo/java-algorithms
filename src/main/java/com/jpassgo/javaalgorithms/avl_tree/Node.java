1f3e31bc35c0a21b1cab44c35591429ac5a1c84d
jpassgo <jeffpascoe9@gmail.com>
2021-09-02 20:08:45 -0500

    Adding methods for updating the balanceFactor and height of a given node. Starting implementation of a method that will update the balance of a tree.

--- src/main/java/com/jpassgo/javaalgorithms/avl_tree/Node.java
+++ src/main/java/com/jpassgo/javaalgorithms/avl_tree/Node.java
@@ -11,8 +11,46 @@ public class Node<T extends Comparable> {
 
     private T value;
     private int balanceFactor;
-    private int heightOfSubTree;
+    private int height;
     private Node leftChild;
     private Node rightChild;
 
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public void updateHeight() {
+        int leftSubTreeHeight = leftChild == null ? -1 : leftChild.height;
+        int rightSubTreeHeight = rightChild == null ? -1 : rightChild.height;
+
+        this.height = 1 + Math.max(leftSubTreeHeight, rightSubTreeHeight);
+    }
+
+    public void updateBalanceFactor() {
+        int leftSubTreeHeight = leftChild == null ? -1 : leftChild.height;
+        int rightSubTreeHeight = rightChild == null ? -1 : rightChild.height;
+
+        this.balanceFactor = rightSubTreeHeight - leftSubTreeHeight;
+    }
+
+    public Node balance() {
+
+        // This indicates a left heavy subtree
+        if(this.getBalanceFactor() == -2) {
+            if(this.getLeftChild().getBalanceFactor() <= 0) {
+                return leftLeftCase(this);
+            } else {
+                return leftRightCase(this);
+            }
+            // This indicates a right heavy subtree
+        } else if(this.getBalanceFactor() == 2)  {
+            if(this.getRightChild().getBalanceFactor() >= 0) {
+                return rightRightCase(this);
+            } else {
+                return rightLeftCase(this);
+            }
+        }
+
+        return this;
+    }
 }
