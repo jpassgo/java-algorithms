f8bf3d4e44a8fc665d13a92fb9296e2d2f160ead
jpassgo <jeffpascoe9@gmail.com>
2021-09-08 20:34:12 -0500

    Spotless formatting.

--- src/main/java/com/jpassgo/javaalgorithms/avl_tree/Node.java
+++ src/main/java/com/jpassgo/javaalgorithms/avl_tree/Node.java
@@ -9,86 +9,86 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Node<T extends Comparable> {
 
-    private T value;
-    private int balanceFactor;
-    private int height;
-    private Node leftChild;
-    private Node rightChild;
+  private T value;
+  private int balanceFactor;
+  private int height;
+  private Node leftChild;
+  private Node rightChild;
 
-    public Node(T value) {
-        this.value = value;
+  public Node(T value) {
+    this.value = value;
+  }
+
+  public void updateHeight() {
+    int leftSubTreeHeight = leftChild == null ? -1 : leftChild.height;
+    int rightSubTreeHeight = rightChild == null ? -1 : rightChild.height;
+
+    this.height = 1 + Math.max(leftSubTreeHeight, rightSubTreeHeight);
+  }
+
+  public void updateBalanceFactor() {
+    int leftSubTreeHeight = leftChild == null ? -1 : leftChild.height;
+    int rightSubTreeHeight = rightChild == null ? -1 : rightChild.height;
+
+    this.balanceFactor = rightSubTreeHeight - leftSubTreeHeight;
+  }
+
+  public Node balance() {
+
+    // This indicates a left heavy subtree
+    if (this.getBalanceFactor() == -2) {
+      if (this.getLeftChild().getBalanceFactor() <= 0) {
+        return leftLeftCase(this);
+      } else {
+        return leftRightCase(this);
+      }
+      // This indicates a right heavy subtree
+    } else if (this.getBalanceFactor() == 2) {
+      if (this.getRightChild().getBalanceFactor() >= 0) {
+        return rightRightCase(this);
+      } else {
+        return rightLeftCase(this);
+      }
     }
 
-    public void updateHeight() {
-        int leftSubTreeHeight = leftChild == null ? -1 : leftChild.height;
-        int rightSubTreeHeight = rightChild == null ? -1 : rightChild.height;
+    return this;
+  }
 
-        this.height = 1 + Math.max(leftSubTreeHeight, rightSubTreeHeight);
-    }
+  private Node rightLeftCase(Node<T> node) {
+    node.setRightChild(rightRotation(node.getRightChild()));
+    return rightRightCase(node);
+  }
 
-    public void updateBalanceFactor() {
-        int leftSubTreeHeight = leftChild == null ? -1 : leftChild.height;
-        int rightSubTreeHeight = rightChild == null ? -1 : rightChild.height;
+  private Node rightRightCase(Node<T> node) {
+    return leftRotation(node);
+  }
 
-        this.balanceFactor = rightSubTreeHeight - leftSubTreeHeight;
-    }
+  private Node leftRightCase(Node<T> node) {
+    node.setLeftChild(leftRotation(node.getLeftChild()));
+    return leftLeftCase(node);
+  }
 
-    public Node balance() {
+  private Node leftLeftCase(Node node) {
+    return rightRotation(node);
+  }
 
-        // This indicates a left heavy subtree
-        if(this.getBalanceFactor() == -2) {
-            if(this.getLeftChild().getBalanceFactor() <= 0) {
-                return leftLeftCase(this);
-            } else {
-                return leftRightCase(this);
-            }
-            // This indicates a right heavy subtree
-        } else if(this.getBalanceFactor() == 2)  {
-            if(this.getRightChild().getBalanceFactor() >= 0) {
-                return rightRightCase(this);
-            } else {
-                return rightLeftCase(this);
-            }
-        }
+  private Node leftRotation(Node node) {
+    Node parent = node.getRightChild();
+    node.setRightChild(parent.getLeftChild());
+    parent.setLeftChild(node);
 
-        return this;
-    }
+    node.updateHeight();
+    node.updateBalanceFactor();
+    return parent;
+  }
 
-    private Node rightLeftCase(Node<T> node) {
-        node.setRightChild(rightRotation(node.getRightChild()));
-        return rightRightCase(node);
-    }
+  private Node rightRotation(Node node) {
+    Node parent = node.getLeftChild();
+    node.setLeftChild(parent.getRightChild());
+    parent.setRightChild(node);
 
-    private Node rightRightCase(Node<T> node) {
-        return leftRotation(node);
-    }
-
-    private Node leftRightCase(Node<T> node) {
-        node.setLeftChild(leftRotation(node.getLeftChild()));
-        return leftLeftCase(node);
-    }
-
-    private Node leftLeftCase(Node node) {
-        return rightRotation(node);
-    }
-
-    private Node leftRotation(Node node) {
-        Node parent = node.getRightChild();
-        node.setRightChild(parent.getLeftChild());
-        parent.setLeftChild(node);
-
-        node.updateHeight();
-        node.updateBalanceFactor();
-        return parent;
-    }
-
-    private Node rightRotation(Node node) {
-        Node parent = node.getLeftChild();
-        node.setLeftChild(parent.getRightChild());
-        parent.setRightChild(node);
-
-        node.updateHeight();
-        node.updateBalanceFactor();
-        return parent;
-    }
+    node.updateHeight();
+    node.updateBalanceFactor();
+    return parent;
+  }
 }
