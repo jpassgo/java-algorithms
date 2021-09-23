739884a3147298539a8f5adaeb4a81363b5cf6d4
jpassgo <jeffpascoe9@gmail.com>
2021-09-22 21:12:54 -0500

    Min Heap implementation.

--- src/main/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeap.java
+++ src/main/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeap.java
@@ -1,23 +1,43 @@
 package com.jpassgo.javaalgorithms.heap.min_heap;
 
-public class MinHeap<T extends Comparable> {
-    private T[] heap;
+public class MinHeap {
+    private int[] heap;
     private int size;
 
     public MinHeap() {
+        heap = new int[10];
         size = 0;
     }
 
     public MinHeap(int size) {
-        this.heap = heap;
-        this.size = size;
+        this.heap = new int[size];
+        this.size = 0;
     }
 
-    public <T> int insert(T value) {
+    public int insert(int value) {
+        if(size == 0) {
+            heap[size] = value;
+            size+=1;
+        } else {
+            heap[size] = value;
+        }
 
-        heap[++size] = value;
+        int index = size;
+        boolean isLessThanParent = true;
+
+        while(isLessThanParent && size > 1) {
+            int parentIndex = (index - 1) / 2;
+            if(heap[index] < heap[parentIndex]) {
+                int temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+                index = parentIndex;
+            } else {
+                isLessThanParent = false;
+            }
+        }
 
 
-        return 0;
+        return value;
     }
 }
---
+++ src/test/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeapTest.java
@@ -0,0 +1,20 @@
+package com.jpassgo.javaalgorithms.heap.min_heap;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MinHeapTest {
+
+    @Test
+    void insert() {
+        MinHeap minHeap = new MinHeap(100);
+        minHeap.insert(78);
+        minHeap.insert(10);
+        minHeap.insert(8);
+        minHeap.insert(90);
+        minHeap.insert(2);
+        minHeap.insert(9);
+    }
+}
