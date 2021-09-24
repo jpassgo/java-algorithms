d0e51c5ed27012d89b3a60cd3cf79943937f4659
jpassgo <jeffpascoe9@gmail.com>
2021-09-23 20:10:25 -0500

    Spotless.

--- src/main/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeap.java
+++ src/main/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeap.java
@@ -1,43 +1,42 @@
 package com.jpassgo.javaalgorithms.heap.min_heap;
 
 public class MinHeap {
-    private int[] heap;
-    private int size;
+  private int[] heap;
+  private int size;
 
-    public MinHeap() {
-        heap = new int[10];
-        size = 0;
+  public MinHeap() {
+    heap = new int[10];
+    size = 0;
+  }
+
+  public MinHeap(int size) {
+    this.heap = new int[size];
+    this.size = 0;
+  }
+
+  public int insert(int value) {
+    if (size == 0) {
+      heap[size] = value;
+      size += 1;
+    } else {
+      heap[size] = value;
     }
 
-    public MinHeap(int size) {
-        this.heap = new int[size];
-        this.size = 0;
+    int index = size;
+    boolean isLessThanParent = true;
+
+    while (isLessThanParent && size > 1) {
+      int parentIndex = (index - 1) / 2;
+      if (heap[index] < heap[parentIndex]) {
+        int temp = heap[index];
+        heap[index] = heap[parentIndex];
+        heap[parentIndex] = temp;
+        index = parentIndex;
+      } else {
+        isLessThanParent = false;
+      }
     }
 
-    public int insert(int value) {
-        if(size == 0) {
-            heap[size] = value;
-            size+=1;
-        } else {
-            heap[size] = value;
-        }
-
-        int index = size;
-        boolean isLessThanParent = true;
-
-        while(isLessThanParent && size > 1) {
-            int parentIndex = (index - 1) / 2;
-            if(heap[index] < heap[parentIndex]) {
-                int temp = heap[index];
-                heap[index] = heap[parentIndex];
-                heap[parentIndex] = temp;
-                index = parentIndex;
-            } else {
-                isLessThanParent = false;
-            }
-        }
-
-
-        return value;
-    }
+    return value;
+  }
 }
--- src/test/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeapTest.java
+++ src/test/java/com/jpassgo/javaalgorithms/heap/min_heap/MinHeapTest.java
@@ -7,14 +7,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class MinHeapTest {
 
-    @Test
-    void insert() {
-        MinHeap minHeap = new MinHeap(100);
-        minHeap.insert(78);
-        minHeap.insert(10);
-        minHeap.insert(8);
-        minHeap.insert(90);
-        minHeap.insert(2);
-        minHeap.insert(9);
-    }
+  @Test
+  void insert() {
+    MinHeap minHeap = new MinHeap(100);
+    minHeap.insert(78);
+    minHeap.insert(10);
+    minHeap.insert(8);
+    minHeap.insert(90);
+    minHeap.insert(2);
+    minHeap.insert(9);
+  }
 }
