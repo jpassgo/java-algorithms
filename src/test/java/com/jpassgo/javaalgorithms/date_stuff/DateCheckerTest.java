6a43c944174f696f14e7aeeb094f37d048297afc
jpassgo <jeffpascoe9@gmail.com>
2021-09-30 21:06:43 -0500

    More Spotless

--- src/test/java/com/jpassgo/javaalgorithms/date_stuff/DateCheckerTest.java
+++ src/test/java/com/jpassgo/javaalgorithms/date_stuff/DateCheckerTest.java
@@ -1,30 +1,26 @@
 package com.jpassgo.javaalgorithms.date_stuff;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 @ExtendWith(MockitoExtension.class)
 class DateCheckerTest {
 
-    @Mock Clock clock;
-    @InjectMocks DateChecker dateChecker;
+  @Mock Clock clock;
+  @InjectMocks DateChecker dateChecker;
 
-    private final static LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2021, 01, 01, 01, 9);
+  private static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2021, 01, 01, 01, 9);
 
-    @Test
-    void isMinuteEven() {
-        clock.fixed(LOCAL_DATE_TIME.toInstant(ZoneOffset.MIN), ZoneId.systemDefault());
+  @Test
+  void isMinuteEven() {
+    clock.fixed(LOCAL_DATE_TIME.toInstant(ZoneOffset.MIN), ZoneId.systemDefault());
 
-        assertThat(dateChecker.isMinuteEven()).isTrue();
-
-    }
-
-
+    assertThat(dateChecker.isMinuteEven()).isTrue();
+  }
 }
